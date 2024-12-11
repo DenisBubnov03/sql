941f6d42b86369a910b5acbc64a4ea33f1f3a677
Denis Bubnov <105424941+DenisBubnov03@users.noreply.github.com>
2024-12-11 20:07:26 +0300

    Update google_sheets.py

--- student_management/google_sheets.py
+++ student_management/google_sheets.py
@@ -3,6 +3,7 @@ from google.oauth2.service_account import Credentials
 
 # Подключение к Google Sheets через OAuth
 def connect_to_sheets():
-    gc = gspread.oauth(credentials_filename="client_secret.json")  # Путь к скачанному OAuth-файлу
+    gc = gspread.service_account(filename="/etc/secrets/client_secret.json")
+  # Путь к скачанному OAuth-файлу
     spreadsheet = gc.open("Ученики")  # Замените "Ученики" на название таблицы
     return spreadsheet.sheet1
