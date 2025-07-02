b87edcdc2321081ceab9a2e8ab0ed427107fe217
Denis_Bubnov <d.bubnov@brlab.net>
2025-07-02 15:32:04 +0300

    fix v4

--- commands/start_commands.py
+++ commands/start_commands.py
@@ -33,16 +33,21 @@ async def exit_to_main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     Возврат в главное меню и завершение ConversationHandler.
     """
-    reply_keyboard = [
-        ["Добавить студента", "Премия куратору"],
-        ["Редактировать данные студента", "Проверить уведомления"],
-        ["Поиск ученика", "Статистика", "📊 Рассчитать зарплату"]
+    user_id = update.message.from_user.id
 
-    ]
+    if user_id not in AUTHORIZED_USERS and user_id not in NOT_ADMINS:
+        await update.message.reply_text("Извините, у вас нет доступа.")
+        return
+
+    # Ограниченное меню для not_admin
+    if user_id in NOT_ADMINS:
+        reply_keyboard = [['Добавить студента', 'Поиск ученика', 'Статистика']]
+    else:
+        reply_keyboard = [
+            ['Добавить студента', 'Премия куратору'],
+            ['Редактировать данные студента', 'Проверить уведомления'],
+            ['Поиск ученика', 'Статистика', "📊 Рассчитать зарплату"]
+        ]
+
     markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
-
-    await update.message.reply_text(
-        "Выберите действие:",
-        reply_markup=markup
-    )
-    return ConversationHandler.END
+    await update.message.reply_text("Выберите действие:", reply_markup=markup)
