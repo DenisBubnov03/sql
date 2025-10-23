a43c18a5d9bbe35d9165057b40d62a481b5c48ed
Denis_Bubnov <d.bubnov@brlab.net>
2025-10-23 15:59:49 +0700

    v 0.25

---
+++ commands/fullstack_constants.py
@@ -0,0 +1,23 @@
+# Константы для расчета ЗП директоров направления фуллстек курса
+
+# Маппинг тем для ручного директора (ID = 1)
+TOPIC_FIELD_MAPPING = {
+    "1 модуль": "m1_submission_date",
+    "Тема 2.1 + 2.2": "m2_1_2_2_submission_date",
+    "Тема 2.3 + 3.1": "m2_3_3_1_submission_date",
+    "Тема 3.2": "m3_2_submission_date",
+    "Тема 3.3": "m3_3_submission_date",
+    "Тема 4.1": "m4_1_submission_date",
+    "Тема 4.2 + 4.3": "m4_2_4_3_submission_date",
+    "Мок экзамен 4 модуля": "m4_mock_exam_passed_date",
+}
+
+# Маппинг модулей для авто директора (ID = 3)
+AUTO_MODULE_FIELD_MAPPING = {
+    "Сдача 2 модуля": "m2_exam_passed_date",
+    "Сдача 3 модуля": "m3_exam_passed_date",
+    "Сдача 4 модуля": "m4_topic_passed_date",
+    "Сдача 5 модуля": "m5_topic_passed_date",
+    "Сдача 6 модуля": "m6_topic_passed_date",
+    "Сдача 7 модуля": "m7_topic_passed_date",
+}
